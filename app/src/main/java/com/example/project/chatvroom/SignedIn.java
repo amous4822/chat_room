0b7475af3a40615970b70404f825e8b885efcf57
amous4822 <amous4822@gmail.com>
2018-10-24 15:15:09 +0530

    Added options and settings activity

--- app/src/main/java/com/example/project/chatvroom/SignedIn.java
+++ app/src/main/java/com/example/project/chatvroom/SignedIn.java
@@ -1,11 +1,16 @@
 package com.example.project.chatvroom;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignedIn extends AppCompatActivity {
 
@@ -13,17 +18,29 @@ public class SignedIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signed_in);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.signin_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+
+            case R.id.account_settings :
+                startActivity(new Intent( this, SettingsActivity.class));
+                return true;
+
+            case R.id.sign_out:
+                FirebaseAuth.getInstance().signOut();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
